c43860bbfdfc13d9fb6f487c2d93f6124313cd25
Ajun5GIT <153270000+Ajun5GIT@users.noreply.github.com>
2024-03-26 23:49:01 +0900

    少し変更

--- Assets/Scripts/allscene/SceneChanger.cs
+++ Assets/Scripts/allscene/SceneChanger.cs
@@ -8,7 +8,10 @@ namespace allscene
 {
     public class SceneChanger : MonoBehaviour
     {
+        
         public void ChangeSceneTo(string sceneName) { SceneManager.LoadScene(sceneName); }
+
+        public void AddScene(string sceneName) { SceneManager.LoadScene(sceneName , LoadSceneMode.Additive); }
 
         public void GameQuit() { Application.Quit(); }
     }
